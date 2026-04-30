efade934df11f37eccb68b4499948bd1f615e99c
isaack mburu <felixmburu03@gmail.com>
2026-05-01 00:15:05 +0300

    convert home and checkout to async await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,11 +8,14 @@ export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
 
-  useEffect(() => {
-      axios.get('/api/products')
-    .then((response) => {
+  useEffect( () => {
+    const getHomeData = async () => {
+    const response = await  axios.get('/api/products');
       setProducts(response.data);
-  });
+    };
+
+    getHomeData();
+
   },[]);
   
 
