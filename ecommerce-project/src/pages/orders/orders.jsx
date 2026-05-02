41cba13014cca7519cbfccf36151ebe63efce1a4
isaack mburu <felixmburu03@gmail.com>
2026-05-02 15:31:57 +0300

    use async to fetch orders data

--- ecommerce-project/src/pages/orders/orders.jsx
+++ ecommerce-project/src/pages/orders/orders.jsx
@@ -10,10 +10,11 @@ export function OrdersPage({ cart }) {
   const [ orders, setOrders ] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/orders?expand=products')
-      .then((response) => {
-        setOrders(response.data)
-      });
+    const fetchOrdersPageData = async () => {
+    const response = await axios.get('/api/orders?expand=products')
+        setOrders(response.data);
+    }
+    fetchOrdersPageData();
   }, []);
 
   return (
