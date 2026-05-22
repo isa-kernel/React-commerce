08c2942ef672c6773a65445936ba08b2f7890a3c
isaack mburu <felixmburu03@gmail.com>
2026-05-22 12:50:44 +0300

    logo

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -13,9 +13,9 @@ export function Header({ cart }) {
         <div className="left-section">
           <Link to="/" className="header-link">
             <img className="logo"
-              src="images/logo-white.png" />
-            <img className="mobile-logo"
               src="images/mobile-logo-white.png" />
+            <img className="mobile-logo"
+              src="images/mobile-logo.png" />
           </Link>
         </div>
 
